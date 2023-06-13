bc48417c4229eb68e0639ed156ec23fc0d1076a9
motey <bleimehl@gmail.com>
2023-06-13 22:56:34 +0200

    wip

--- hsbt/rclone_config_manager.py
+++ hsbt/rclone_config_manager.py
@@ -1,5 +1,5 @@
 from pathlib import Path, PurePath
-import tempfile
+import logging
 from typing import List, Union
 from dataclasses import dataclass
 from pydantic import BaseModel
@@ -10,67 +10,35 @@ from Configs import getConfig
 from hsbt.key_manager import KeyManager
 from hsbt.storage_box_manager import HetznerStorageBox
 from hsbt.utils import ConfigFileEditor
+from hsbt.utils import cast_path, run_command
+
+log = logging.getLogger(__name__)
 
 
 class Rclone:
-    class RcloneBinary(BaseModel):
-        version: str
-        path: Path
-
-    class RcloneRemoteConfig(BaseModel):
-        identifier: str
-        config_str: str
-        config_file_path: Path = None
-
-        def write(self, file_path: Union[Path, str]):
-            if isinstance(file_path, str):
-                file_path = Path(file_path)
-            file_path.parent.mkdir(parents=True, exist_ok=True)
-            with open(file_path, "wt") as f:
-                f.write(self.config_str)
-            self.config_file_path = file_path
-
-    def __init__(self, storage_box_manager: HetznerStorageBox):
+    def __init__(
+        self,
+        storage_box_manager: HetznerStorageBox,
+        config_file_path: str | Path = None,
+    ):
         self.storage_box_manager = storage_box_manager
+        self.config_file_path: Path = cast_path(config_file_path)
 
     # TODO you are here
     def generate_config_file(self):
-        config_str = inspect.cleandoc(
-            f"""
-                    [{self.key_manager.identifier}]
-                    type = sftp
-                    host = {self.storage_box_manager.host}
-                    user = {self.storage_box_manager.user}
-                    known_hosts_file = {self.storage_box_manager.key_manager.known_host_path}
-                    port = 23
-                    key_file = {self.storage_box_manager.key_manager.private_key_path}
-        """
+        options = dict(
+            type="sftp",
+            host=self.storage_box_manager.host,
+            user=self.storage_box_manager.user,
+            known_hosts_file=self.storage_box_manager.key_manager.known_host_path,
+            port="23",
+            key_file=self.storage_box_manager.key_manager.private_key_path,
         )
-        ConfigFileEditor(target_file=?)
-        config_file_path = Path(self.storage_box_manager.identifier)
-
-        self.remote_config = Rclone.RcloneRemoteConfig(
-            identifier=identifier,
-            config_str=config_str,
-        )
-        self.remote_config.write(config_file_path)
-
-    def generate_config_file_from_hetzner_storage_box(
-        self, storage_box: HetznerStorageBox
-    ):
-        self.generate_config_file(
-            identifier=f"remote-{storage_box.user}",
-            host=storage_box.host,
-            user=storage_box.user,
-            known_host_file=CONFIG.SSH_KNOWN_HOST_FILE,
-            private_key_file=storage_box.private_key_file_openssh,
-            public_key_file=storage_box.public_key_file,
-        )
-
-    def ls_storage_box(self, remote_path="", verbose: bool = False):
-        return run_command(
-            f"""{self.active_binary_version.path} --config="{self.remote_config.config_file_path.absolute()}" ls {self.remote_config.identifier}:{remote_path} {' --verbose' if verbose else ''}"""
-        )
+        # https://rclone.org/commands/rclone_config_create/
+        command = f"""rclone {'--config="' + str(self.config_file_path) + '"' if self.config_file_path else ''} config create {self.storage_box_manager.key_manager.identifier} sftp {' '.join(k+' "'+str(v)+'"' for k,v in options.items())}"""
+        log.debug("Create rclone config")
+        run_command(command)
+        return command
 
     def bisync_storage_box(
         self,
@@ -79,6 +47,7 @@ class Rclone:
         resync: bool = False,
         verbose: bool = False,
     ):
+        raise NotImplementedError()
         if isinstance(local_dir, str):
             local_dir = Path(local_dir)
         local_dir.mkdir(exist_ok=True, parents=True)
@@ -89,6 +58,10 @@ class Rclone:
     def sync_from_storage_box_to_local(
         self, local_dir: Union[str, Path], remote_path="/", verbose: bool = False
     ):
+        raise NotImplementedError()
         return run_command(
             f"""{self.active_binary_version.path} --config="{self.remote_config.config_file_path.absolute()}" sync {self.remote_config.identifier}:{remote_path} {local_dir}{' --verbose' if verbose else ''}{' --filters-file' + CONFIG.RCLONE_SYNC_FILTER if CONFIG.RCLONE_SYNC_FILTER else ''}"""
         )
+
+    def mount(self, local_dir: str):
+        raise NotImplementedError()
